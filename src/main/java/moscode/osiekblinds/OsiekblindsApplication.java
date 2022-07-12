93aaf8aee33eb01c4499a93cecc428657af8b168
Pawel Moskwa <pawel.moskwa@source-corp.com>
2022-07-12 16:18:30 +0200

    initial commit

---
+++ src/main/java/moscode/osiekblinds/OsiekblindsApplication.java
@@ -0,0 +1,13 @@
+package moscode.osiekblinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsiekblindsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsiekblindsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moscode/osiekblinds/OsiekblindsApplicationTests.java
@@ -0,0 +1,13 @@
+package moscode.osiekblinds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsiekblindsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
