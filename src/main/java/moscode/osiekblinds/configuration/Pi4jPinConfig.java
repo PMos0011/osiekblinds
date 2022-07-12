6ea013fb26f7f923ecf1bb688e85b788d3e35b82
Pawel Moskwa <pawel.moskwa@source-corp.com>
2022-07-12 22:44:37 +0200

    system path test

--- src/main/java/moscode/osiekblinds/configuration/Pi4jPinConfig.java
+++ src/main/java/moscode/osiekblinds/configuration/Pi4jPinConfig.java
@@ -12,7 +12,6 @@ public class Pi4jPinConfig {
 
     @Bean
     public Context pi4j() {
-        System.setProperty("pi4j.library.path", "/home/pi");
         Context pi4j = Pi4J.newAutoContext();
 
         var led = DigitalOutput.newConfigBuilder(pi4j)
