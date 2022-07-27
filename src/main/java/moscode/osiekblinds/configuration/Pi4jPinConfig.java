7b03a45bf093515ecdf2184616e681f881ace984
Pawel Moskwa <pawel.moskwa@source-corp.com>
2022-07-27 20:50:07 +0200

    pinout change

--- src/main/java/moscode/osiekblinds/configuration/Pi4jPinConfig.java
+++ src/main/java/moscode/osiekblinds/configuration/Pi4jPinConfig.java
@@ -13,26 +13,26 @@ import java.util.Map;
 public class Pi4jPinConfig {
 
     private final static Map<String, Integer> outputs = Map.ofEntries(
-            Map.entry("rel-u-0", 17),
-            Map.entry("rel-d-0", 18),
-            Map.entry("rel-u-1", 22),
-            Map.entry("rel-d-1", 24),
-            Map.entry("rel-u-2", 19),
-            Map.entry("rel-d-2", 9),
-            Map.entry("rel-u-3", 25),
-            Map.entry("rel-d-3", 11),
-            Map.entry("rel-u-4", 8),
-            Map.entry("rel-d-4", 7),
-            Map.entry("rel-u-5", 6),
-            Map.entry("rel-d-5", 12),
-            Map.entry("rel-u-6", 13),
-            Map.entry("rel-d-6", 19),
-            Map.entry("rel-u-7", 16),
-            Map.entry("rel-d-7", 26),
-            Map.entry("rel-u-8", 20),
-            Map.entry("rel-d-8", 21),
-            Map.entry("rel-u-9", 27),
-            Map.entry("rel-d-9", 15)
+            Map.entry("rel-u-0", 15),
+            Map.entry("rel-d-0", 17),
+            Map.entry("rel-u-1", 18),
+            Map.entry("rel-d-1", 27),
+            Map.entry("rel-u-2", 22),
+            Map.entry("rel-d-2", 23),
+            Map.entry("rel-u-3", 24),
+            Map.entry("rel-d-3", 10),
+            Map.entry("rel-u-4", 9),
+            Map.entry("rel-d-4", 25),
+            Map.entry("rel-u-5", 11),
+            Map.entry("rel-d-5", 8),
+            Map.entry("rel-u-6", 5),
+            Map.entry("rel-d-6", 6),
+            Map.entry("rel-u-7", 12),
+            Map.entry("rel-d-7", 13),
+            Map.entry("rel-u-8", 19),
+            Map.entry("rel-d-8", 16),
+            Map.entry("rel-u-9", 26),
+            Map.entry("rel-d-9", 20)
     );
 
     @Bean
@@ -50,7 +50,7 @@ public class Pi4jPinConfig {
                 .provider("pigpio-digital-output")
                 .shutdown(DigitalState.LOW)
                 .initial(DigitalState.HIGH)
-                .address(4)
+                .address(14)
                 .id("rel-power"));
 
         return pi4j;
