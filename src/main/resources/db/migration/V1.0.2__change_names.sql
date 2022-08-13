fd20394e6c2747ab7dc768c8808cedf374e9ecc6
Pawel Moskwa <pawel.moskwa@source.com>
2022-08-13 15:26:55 +0200

    loading spinner added

---
+++ src/main/resources/db/migration/V1.0.2__change_names.sql
@@ -0,0 +1,5 @@
+update blinds_definitions set blind_name = "Biuro" where id = 9;
+update blinds_definitions set blind_name = "Łazienka" where id = 8;
+update blinds_definitions set blind_name = "Sypialnia" where id = 7;
+update blinds_definitions set blind_name = "Pokój" where id = 6;
+update blinds_definitions set blind_name = "Garderoba" where id = 5;
--- ui/src/componnents/BlindsNavigation.tsx
+++ ui/src/componnents/BlindsNavigation.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useContent } from './ContentProvider';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 import BlindSelector from './BlindSelector';
 
 const Container = styled.div`
@@ -19,17 +21,30 @@ const SelectorContainer = styled.div`
 
 const BlindsNavigation = () => {
   const { blinds } = useContent();
-  return (
-    <Container>
-      {blinds.map((blind) => {
-        return (
-          <SelectorContainer key={blind.id}>
-            <BlindSelector label={blind.blindName} id={blind.id} doubleArrow={blind.global} />
-          </SelectorContainer>
-        );
-      })}
-    </Container>
-  );
+
+  const DisplayBlinds = () => {
+    return (
+      <Container>
+        {blinds.map((blind) => {
+          return (
+            <SelectorContainer key={blind.id}>
+              <BlindSelector label={blind.blindName} id={blind.id} doubleArrow={blind.global} />
+            </SelectorContainer>
+          );
+        })}
+      </Container>
+    );
+  };
+
+  const Spinner = () => {
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+      </Box>
+    );
+  };
+
+  return blinds.length === 0 ? Spinner() : DisplayBlinds();
 };
 
 export default BlindsNavigation;
